9ab530648dcbcaf0c934ae5188feb074e4303f35
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2020-06-17 11:39:59 +0200

    First commit

---
+++ app/src/main/java/es/fjruiz/base/MainActivity.kt
@@ -0,0 +1,18 @@
+package es.fjruiz.base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.fjruiz.base.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
