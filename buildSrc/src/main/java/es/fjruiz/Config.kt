47ae71b73fb602ce22c2c7d23cabaa3fe07263fc
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2020-06-17 12:36:07 +0200

    Add buildSrc and ktlint

---
+++ buildSrc/src/main/java/es/fjruiz/Config.kt
@@ -0,0 +1,5 @@
+object Config {
+    val compileSdkVersion = 28
+    val targetSdkVersion = 28
+    val minSdkVersion = 21
+}
---
+++ buildSrc/src/main/java/es/fjruiz/Dependencies.kt
@@ -0,0 +1,12 @@
+package es.fjruiz
+
+private const val ANDROID_BUILD_TOOL_VERSION = "4.0.0"
+private const val KOTLIN_VERSION = "1.3.72"
+private const val KTLINT_VERSION = "2.4.0"
+
+object Dependencies {
+    val androidBuildToolGradlePlugin = "com.android.tools.build:gradle:$ANDROID_BUILD_TOOL_VERSION"
+    val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+    val kotlinSTDLib = "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION"
+    val ktlintPlugin = "org.jmailen.gradle:kotlinter-gradle:$KTLINT_VERSION"
+}
